6d724bb07134a48378b4936bf4cfaa89ae743af1
jaeroot <jaeroot98@gmail.com>
2023-03-21 09:58:34 +0900

    Unreal CPP Study

---
+++ Source/ArenaBattle/Private/ABGameMode.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABGameMode.h"
+#include "ABCharacter.h"
+#include "ABPlayerController.h"
+#include "ABPlayerState.h"
+
+AABGameMode::AABGameMode()
+{
+	DefaultPawnClass = AABCharacter::StaticClass();
+	PlayerControllerClass = AABPlayerController::StaticClass();
+	//static ConstructorHelpers::FClassFinder<APawn> BP_PAWN_C(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	//if (BP_PAWN_C.Succeeded())
+	//{
+	//	DefaultPawnClass = BP_PAWN_C.Class;
+	//}
+	PlayerStateClass = AABPlayerState::StaticClass();
+}
+
+void AABGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	ABLOG(Warning, TEXT("PostLogin Begin"));
+	Super::PostLogin(NewPlayer);
+	ABLOG(Warning, TEXT("PostLogin End"));
+
+	auto ABPlayerState = Cast<AABPlayerState>(NewPlayer->PlayerState);
+	ABCHECK(nullptr != ABPlayerState);
+	ABPlayerState->InitPlayerData();
+}
---
+++ Source/ArenaBattle/Private/ABPlayerController.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABPlayerController.h"
+#include "ABHUDWidget.h"
+#include "ABPlayerState.h"
+
+AABPlayerController::AABPlayerController()
+{
+	static ConstructorHelpers::FClassFinder<UABHUDWidget> UI_HUD_C(TEXT("/Game/Book/UI/UI_HUD.UI_HUD_C"));
+	if (UI_HUD_C.Succeeded())
+	{
+		HUDWidgetClass = UI_HUD_C.Class;
+	}
+}
+
+void AABPlayerController::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	ABLOG_S(Warning);
+}
+
+void AABPlayerController::OnPossess(APawn* aPawn)
+{
+	ABLOG_S(Warning);
+	Super::OnPossess(aPawn);
+}
+
+void AABPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FInputModeGameOnly InputMode;
+	SetInputMode(InputMode);
+
+	HUDWidget = CreateWidget<UABHUDWidget>(this, HUDWidgetClass);
+	HUDWidget->AddToViewport();
+
+	auto ABPlayerState = Cast<AABPlayerState>(PlayerState);
+	ABCHECK(nullptr != ABPlayerState);
+	HUDWidget->BindPlayerState(ABPlayerState);
+	ABPlayerState->OnPlayerStateChanged.Broadcast();
+}
+
+UABHUDWidget* AABPlayerController::GetHUDWidget() const
+{
+	return HUDWidget;
+}
